13ad9e0f19ed008313c21135af68787a39c5b97c
Richard Chen <chenpipi@live.com>
2018-12-08 12:45:02 -0500

    js

---
+++ main.js
@@ -0,0 +1,22 @@
+var neighbourhoods;
+var slider=[neighbourhoods];
+var userScore=0;
+var personalProfile={
+    Name: name,
+    Age: age,
+    Occupation: occupation,
+    Ethnicity: ethnicity,
+    Family :familySize,
+    HouseholdIncome:income,
+    Residential:residentialStatus
+}
+
+function personaLProfile(name,age,occupation,ethnicity,familySize,income,residentialStatus){
+    this.name=name;
+    this.name=age;
+    this.occupation=occupation;
+    this.ethnicity=ethnicity;
+    this.familySize=familySize;
+    this.income=income;
+    this.residentialStatus=residentialStatus;
+}
